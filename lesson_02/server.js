e07bbcd8b38d0a9ef812b6bf6958d41a245a5f81
Shubham Kunal <shubhamkunal19@gmail.com>
2022-12-18 01:37:23 +0530

    L2

--- lesson_02/server.js
+++ lesson_02/server.js
@@ -2,9 +2,20 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3500
+const {logger} = require('./middleware/logger')
+const {errorHandler} = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 
+app.use(logger)
+app.use(cookieParser())
+app.use(cors(corsOptions))
 app.use('/',express.static(path.join(__dirname, 'public')))
 app.use('/', require("./routes/root"))
+app.use(express.json())
+
+
 
 app.all("*",(req,res)=>{
     res.status(404)
@@ -17,6 +28,7 @@ app.all("*",(req,res)=>{
     }
 })
 
+// app.use(errorHandler)
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
